40f923acde52d9e497f78d2ee3e218243c839d7e
Ismail Ghadban <1006878@student.sae.edu.au>
2016-09-27 22:25:32 +1000

    Literally just needs to look pretty

---
+++ Assets/Scripts/PowerUpBlock.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpBlock : MonoBehaviour
+{
+
+    public BoxCollider2D boxcollider;
+    public GameObject spawnPoint;
+    public GameObject spawnedPrefab;
+
+    public bool spawned = false;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+
+        if (col.tag == "Player" && spawned == false)
+        {
+            if (spawnedPrefab)
+            {
+                Instantiate(spawnedPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+            }
+            Debug.Log("memed");
+            spawned = true;
+        }
+    }
+}
